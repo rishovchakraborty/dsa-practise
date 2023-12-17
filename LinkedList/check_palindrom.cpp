174544c425dbe6cf92e27aa528300201df077f11
rishov <rishon3003@gmail.com>
2023-12-17 23:46:31 +0530

    day5

--- LinkedList/check_palindrom.cpp
+++ LinkedList/check_palindrom.cpp
@@ -50,3 +50,46 @@ public:
         cout << "null" << endl;
     }
 };
+bool isPalindrom(Node* &head){
+    //find mid ele
+    Node* slow=head;
+    Node* fast=head;
+    while(fast&&fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    //slow pointr poniting to middle element
+    Node* curr=slow->next;
+    Node* prev=slow;
+    slow->next=NULL;
+
+    //reverse the list
+    while(curr){
+        Node* next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+    }
+    //check through thr reverse ll
+    Node* h1=head;
+    Node* h2=prev;
+    while(h2){
+        if(h1->val!=h2->val){
+            return false;
+        }
+        h1=h1->next;
+        h2=h2->next;
+    }
+    return true;
+}
+
+int main(){
+    LinkedList ll;
+    ll.insertAtTail(1);
+    ll.insertAtTail(2);
+    ll.insertAtTail(3);
+    ll.insertAtTail(4);
+    ll.insertAtTail(1);
+    ll.display();
+    cout<<isPalindrom(ll.head);
+}
---
+++ LinkedList/odd_even_LL.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int data)
+    {
+        val = data;
+        next = NULL;
+    }
+};
+
+class LinkedList
+{
+public:
+    Node *head;
+    LinkedList()
+    {
+        head = NULL;
+    }
+
+    void insertAtTail(int val)
+    {
+        Node *new_node = new Node(val);
+        if (!head)
+        {
+            head = new_node;
+            return;
+        }
+        Node *temp = head;
+        while (temp->next)
+        {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
+
+    void display()
+    {
+        Node *temp = head;
+        while (temp)
+        {
+            cout << temp->val << "-->";
+            temp = temp->next;
+        }
+        cout << "null" << endl;
+    }
+};
+
+Node* oddEven(Node* &head){
+    Node* evenHead=head->next;
+    Node* even=evenHead;
+    Node* odd=head;
+    while(even&&even->next){
+        odd->next=odd->next->next;
+        even->next=even->next->next;
+        odd=odd->next;
+        even=even->next;
+    }
+    odd->next=evenHead;
+    return head;
+}
